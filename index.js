3930fac31a5dce69dace27bab6eaea828cc23c3d
Asim <saffarrli14@gmail.com>
2024-03-21 12:35:52 +0400

    add

---
+++ index.js
@@ -0,0 +1,8 @@
+
+import expres from 'express'
+
+const app = express();
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
