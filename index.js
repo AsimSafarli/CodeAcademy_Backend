1c21381b0415de38925295b6aeb5d43fd08917b5
Asim <saffarrli14@gmail.com>
2024-03-21 12:39:06 +0400

    add

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 
-import expres from 'express'
+import express from 'express'
+import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+dotenv.config();
+app.use(bodyParser.json());
+
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
